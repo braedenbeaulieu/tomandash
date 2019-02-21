8688a6a2db4431bc4babe100b94dce2573fdcec8
Braeden Beaulieu <braedenbeaulieu@gmail.com>
2019-02-21 09:48:34 -0500

    Added rough styling for posts index

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,16 +1,28 @@
 @extends('master')
 
 @section('content')
+    <section id="subtitle">
+        <h2>Guestbook</h2>
+    </section>
 
     <section id="blog">
         <section id="posts">
             <div id="makePost">
-                <textarea></textarea>
+                <input type="text" placeholder="Tell us what you're thinking...">
+                <input type="button" value="Post">
             </div>
             @forelse($posts as $post)
                 <div id="post">
-                    <p id="author"><span>{{$post->getAuthor($post)}}</span></p>
-                    <p id="body">{{$post->body}}</p>
+                    <img src="https://fillmurray.com/50/50">
+                    <div>
+                        <p id="author"><span>{{$post->getAuthor($post)}}</span></p>
+                        <p id="postBody">{{$post->body}}</p>
+                        <div id="commentLike">
+                            <p id="likeCounter">100</p>
+                            <input id="like" type="button" value="Like">
+                            <input id="comment" type="button" value="Comment">
+                        </div>
+                    </div>
                 </div>
             @empty
                 <p>There are no posts yet.</p>
