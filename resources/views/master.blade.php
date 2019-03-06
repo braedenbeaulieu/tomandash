d8889755b8563ce27536deb0e35d9bbb44c51d25
Braeden <braedenbeaulieu@gmail.com>
2019-03-06 09:16:50 -0500

    Starting work on blog 2.0

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -39,7 +39,7 @@
 </section>
 
 <footer>
-    <p>all content &copy; 2018</p>
+    <p>all content &copy; 2019</p>
 </footer>
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
