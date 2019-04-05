1212a626ddbf2867ac2e231eb07318e510bcbdf5
Braeden Beaulieu <braedenbeaulieu@gmail.com>
2019-04-05 16:48:20 -0400

    committing

--- resources/js/rsvp.js
+++ resources/js/rsvp.js
@@ -1,67 +1,67 @@
-$(document).ready( function() {
-
-// RSVP Guests
-    $('#guestname2').hide();
-    $('#guestname3').hide();
-    $('#guestname4').hide();
-    $('#guestname5').hide();
-
-
-    $('#guests').change(function(){
-        console.log
-        let guests = +$('#guests').val();
-        let guest2 = $('#guestname2');
-        let guest3 = $('#guestname3');
-        let guest4 = $('#guestname4');
-        let guest5 = $('#guestname5');
-        console.log(guests);
-
-
-        switch(guests) {
-            case 2:
-                guest2.show();
-                break;
-
-            case 3:
-                guest2.show();
-                guest3.show();
-                break;
-
-            case 4:
-                guest2.show();
-                guest3.show();
-                guest4.show();
-                break;
-
-            case 5:
-                guest2.show();
-                guest3.show();
-                guest4.show();
-                guest5.show();
-                break;
-
-            default:
-                console.log('asdad');
-                break;
-        }
-
-        //let guests = $(this).children("option:selected").val();
-        //
-        //
-        // $('#guestname2').addClass('hidden').prop('disabled', true);
-        // $('#guestname3').addClass('hidden').prop('disabled', true);
-        // $('#guestname4').addClass('hidden').prop('disabled', true);
-        // $('#guestname5').addClass('hidden').prop('disabled', true);
-        //
-        // if (guests >= 2) { $('#guestname2').removeClass('hidden').prop('disabled', false); }
-        // if (guests >= 3) { $('#guestname3').removeClass('hidden').prop('disabled', false); }
-        // if (guests >= 4) { $('#guestname4').removeClass('hidden').prop('disabled', false); }
-        // if (guests >= 5) { $('#guestname5').removeClass('hidden').prop('disabled', false); }
-
-    });
-
-    // for tooltip
-    $('.fa-question-circle').tooltip();
-
-
-});
+// $(document).ready( function() {
+//
+// // RSVP Guests
+//     $('#guestname2').hide();
+//     $('#guestname3').hide();
+//     $('#guestname4').hide();
+//     $('#guestname5').hide();
+//
+//
+//     $('#guests').change(function(){
+//         console.log
+//         let guests = +$('#guests').val();
+//         let guest2 = $('#guestname2');
+//         let guest3 = $('#guestname3');
+//         let guest4 = $('#guestname4');
+//         let guest5 = $('#guestname5');
+//         console.log(guests);
+//
+//
+//         switch(guests) {
+//             case 2:
+//                 guest2.show();
+//                 break;
+//
+//             case 3:
+//                 guest2.show();
+//                 guest3.show();
+//                 break;
+//
+//             case 4:
+//                 guest2.show();
+//                 guest3.show();
+//                 guest4.show();
+//                 break;
+//
+//             case 5:
+//                 guest2.show();
+//                 guest3.show();
+//                 guest4.show();
+//                 guest5.show();
+//                 break;
+//
+//             default:
+//                 console.log('asdad');
+//                 break;
+//         }
+//
+//         //let guests = $(this).children("option:selected").val();
+//         //
+//         //
+//         // $('#guestname2').addClass('hidden').prop('disabled', true);
+//         // $('#guestname3').addClass('hidden').prop('disabled', true);
+//         // $('#guestname4').addClass('hidden').prop('disabled', true);
+//         // $('#guestname5').addClass('hidden').prop('disabled', true);
+//         //
+//         // if (guests >= 2) { $('#guestname2').removeClass('hidden').prop('disabled', false); }
+//         // if (guests >= 3) { $('#guestname3').removeClass('hidden').prop('disabled', false); }
+//         // if (guests >= 4) { $('#guestname4').removeClass('hidden').prop('disabled', false); }
+//         // if (guests >= 5) { $('#guestname5').removeClass('hidden').prop('disabled', false); }
+//
+//     });
+//
+//     // for tooltip
+//     $('.fa-question-circle').tooltip();
+//
+//
+// });
